a13b2c082c58b9473720741d9055f9b53b29eca2
Mark Roukema <mroukema@gmail.com>
2023-10-13 15:14:26 -0400

    update tapping term value

--- users/mroukema/config.h
+++ users/mroukema/config.h
@@ -21,21 +21,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TIMEOUT_TO_DEFAULT_LAYER 1000 * 60 * 3
 
 #define TAPPING_TOGGLE 1
-#define TAPPING_TERM 225
+#define TAPPING_TERM 180
 
 #define COMBO_MUST_HOLD_MODS
 #define COMBO_HOLD_TERM 140
 #define COMBO_ONLY_FROM_LAYER 0
 
-#undef ONESHOT_TIMEOUT
 #define ONESHOT_TIMEOUT 3000
 
 // Mouse Key Config
 #define MK_KINETIC_SPEED
 #define MOUSEKEY_INTERVAL 16
 
-#undef MOUSEKEY_MAX_SPEED
-// #define MOUSEKEY_MAX_SPEED 8
+#define MOUSEKEY_MAX_SPEED 14
 
 #define MOUSEKEY_MOVE_DELTA 10
 #define MOUSEKEY_INITIAL_SPEED 140
@@ -43,8 +41,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_DECELERATED_SPEED 800
 #define MOUSEKEY_ACCELERATED_SPEED 2300
 
-#undef MOUSEKEY_TIME_TO_MAX
 #define MOUSEKEY_TIME_TO_MAX 15
 
-#undef MOUSEKEY_WHEEL_TIME_TO_MAX
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 15
